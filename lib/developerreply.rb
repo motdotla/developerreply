eb297f15e8758496cea483aec859fde39c0f4265
scottmotte <scott@scottmotte.com>
2014-08-28 20:16:39 -0700

    Update to use proper API

--- lib/developerreply.rb
+++ lib/developerreply.rb
@@ -38,53 +38,41 @@ module Developerreply
   end
 
   def create(options={})
-    cookie  = attempt_login_and_return_cookie
-
     hostname  = options[:hostname]
     url       = options[:url]
-    payload   = "reply_option[hostname]=#{hostname}&reply_option[url]=#{url}"
+    payload   = { 
+      hostname: hostname, 
+      url: url,
+      api_user: Developerreply.username, 
+      api_key: Developerreply.password
+    }
 
     resp = Developerreply.request.post do |req|
-      req.path    = "/developer/reply"
+      req.path    = "/api/parse.set.json"
       req.body    = payload
-      req.headers["Cookie"] = cookie
     end
 
-    if resp.status == 302 # for SendGrid's response here, a 302 is a successful create... yea, I know.
-      return true
+    if resp.body["error"]
+      error_message = resp.body["error"]["message"] 
+      raise "Unable to create hostname and url: #{error_message}"
     else
-      raise "Unable to create hostname and url at /developer/reply"
+      return true
     end
   end
 
   private
 
-  def login_payload
-    "login[username]=#{Developerreply.username}&login[password]=#{Developerreply.password}"
-  end
-
-  def attempt_login_and_return_cookie
-    cookie = ""
-
-    resp = Developerreply.request.post '/login',  login_payload
-
-    if resp.status == 302 # for SendGrid's auth, a 302 is a successful login where a 200 is not successful... yeah, I know.
-      set_cookie_header = resp.headers["set-cookie"]
-      cookie = set_cookie_header.split(";")[0]
-    end
-
-    raise "Authentication failed" if cookie == ""
-
-    cookie
-  end
-
   def setup_request! 
     options = { 
-      :url      =>  "https://sendgrid.com",
+      #:headers  =>  {'Accept' => "application/json"},
+      :url      =>  "https://api.sendgrid.com",
       :ssl      =>  {:verify => false} 
     } 
  
     Developerreply.request = ::Faraday::Connection.new(options) do |builder| 
+      builder.use     ::Faraday::Request::UrlEncoded
+      builder.use     ::FaradayMiddleware::ParseJson
+      builder.use     ::FaradayMiddleware::FollowRedirects
       builder.adapter ::Faraday.default_adapter 
     end
   end
